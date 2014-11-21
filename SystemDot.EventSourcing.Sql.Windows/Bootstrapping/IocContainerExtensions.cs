5a2ababfac8d3ae55ae5ce1ae44e9f601f821148
sammosampson <sammosampson@gmail.com>
2014-11-21 09:25:49 +0000

    Fixed broken components

--- SystemDot.EventSourcing.Sql.Windows/Bootstrapping/IocContainerExtensions.cs
+++ SystemDot.EventSourcing.Sql.Windows/Bootstrapping/IocContainerExtensions.cs
@@ -3,11 +3,16 @@ using SystemDot.Ioc;
 
 namespace SystemDot.EventSourcing.Sql.Windows.Bootstrapping
 {
+    using SystemDot.EventSourcing.Sessions;
+
     public static class IocContainerExtensions
     {
         internal static void RegisterSqlWindowsEventSourcing(this IIocContainer container)
         {
+            container.RegisterInstance<IAggregateLookup, AggregateLookup>();
+            container.RegisterInstance<IAggregateLookupDataEngine, SqlEventStoreAggregateLookupDataEngine>();
             container.RegisterInstance<ILookupIdCache, LookupIdCache>();
+            container.RegisterInstance<IEventSessionFactory, EventStoreEventSessionFactory>();
         }
     }
 }
