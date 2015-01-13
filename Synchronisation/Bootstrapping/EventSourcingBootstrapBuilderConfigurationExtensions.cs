fe4428350dfce62738ca3828a50c5c6c4d766be2
sammosampson <sammosampson@gmail.com>
2015-01-13 22:52:45 +0000

    Removed unneeded cmd handler reg with dispatcher

--- Synchronisation/Bootstrapping/EventSourcingBootstrapBuilderConfigurationExtensions.cs
+++ Synchronisation/Bootstrapping/EventSourcingBootstrapBuilderConfigurationExtensions.cs
@@ -1,13 +1,8 @@
-﻿using SystemDot.Bootstrapping;
-using SystemDot.Domain.Bootstrapping;
-using SystemDot.Domain.Commands;
+﻿using SystemDot.Domain.Commands;
 using SystemDot.Domain.Queries;
 using SystemDot.EventSourcing.Bootstrapping;
-using SystemDot.EventSourcing.Synchronisation.Client;
 using SystemDot.EventSourcing.Synchronisation.Client.Http;
 using SystemDot.EventSourcing.Synchronisation.Server;
-using SystemDot.Messaging.Handling.Configuration;
-using SystemDot.Messaging.Simple;
 
 namespace SystemDot.EventSourcing.Synchronisation.Bootstrapping
 {
@@ -18,8 +13,7 @@ namespace SystemDot.EventSourcing.Synchronisation.Bootstrapping
             config.GetBootstrapBuilderConfiguration()
                 .RegisterBuildAction(c => c.RegisterInstance<IHttpClientFactory, HttpClientFactory>())
                 .RegisterBuildAction(c => c.RegisterQueryHandlersFromAssemblyOf<CommitQuery>())
-                .RegisterBuildAction(c => c.RegisterCommandHandlersFromAssemblyOf<CommitQuery>())
-                .RegisterBuildAction(c => c.RegisterOpenTypeHandlersWithMessenger(typeof(IAsyncCommandHandler<>)), BuildOrder.Ultimate);
+                .RegisterBuildAction(c => c.RegisterCommandHandlersFromAssemblyOf<CommitQuery>());
 
             return config;
         }
