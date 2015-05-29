152c2f9d96474d3e47c308eea8d944e84ec33d76
sammosampson <sammosampson@gmail.com>
2015-05-29 16:21:20 +0100

    Added Sqlite test app

---
+++ SystemDot.EventSourcing.Sqlite.Android.TestApp/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SystemDot.EventSourcing.Sqlite.Android.TestApp
+{
+    [Activity(Label = "SystemDot.EventSourcing.Sqlite.Android.TestApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate
+            {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
+        }
+    }
+}
+
